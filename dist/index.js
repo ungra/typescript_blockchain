f667f7f7c4a83ab0117794d8deef98ca726c8476
junhee_lee2 <junhee_lee2@tmax.co.kr>
2021-05-11 19:41:12 +0900

    #5 Interfaces on Typescript

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+const person = {
+    name: "junhee",
+    age: 22,
+    gender: "male",
+};
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+console.log(sayHi(person));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "junhee",
+  age: 22,
+  gender: "male",
+};
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
